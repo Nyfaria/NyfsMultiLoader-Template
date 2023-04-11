03b6a775c0af1224daadc6e50df82d925ad5f067
Nyfaria DragonMoose <kinaria3324@gmail.com>
2023-04-11 09:34:30 -0500

    a lot more setup
    Took 25 minutes

--- Common/src/main/java/com/nyfaria/nyfsmultiloader/CommonClass.java
+++ Common/src/main/java/com/nyfaria/nyfsmultiloader/CommonClass.java
@@ -1,5 +1,8 @@
 package com.nyfaria.nyfsmultiloader;
 
+import com.nyfaria.nyfsmultiloader.init.BlockInit;
+import com.nyfaria.nyfsmultiloader.init.EntityInit;
+import com.nyfaria.nyfsmultiloader.init.ItemInit;
 import com.nyfaria.nyfsmultiloader.platform.Services;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
@@ -14,18 +17,8 @@ public class CommonClass {
     // write the majority of your code here and load it from your loader specific projects. This example has some
     // code that gets invoked by the entry point of the loader specific projects.
     public static void init() {
-
-        Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
-        Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
-
-        // It is common for all supported loaders to provide a similar feature that can not be used directly in the
-        // common code. A popular way to get around this is using Java's built-in service loader feature to create
-        // your own abstraction layer. You can learn more about this in our provided services class. In this example
-        // we have an interface in the common code and use a loader specific implementation to delegate our call to
-        // the platform specific approach.
-        if (Services.PLATFORM.isModLoaded("examplemod")) {
-
-            Constants.LOG.info("Hello to examplemod");
-        }
+        ItemInit.loadClass();
+        BlockInit.loadClass();
+        EntityInit.loadClass();
     }
 }
