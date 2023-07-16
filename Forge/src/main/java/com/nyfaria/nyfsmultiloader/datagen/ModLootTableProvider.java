babd8d02db938327cdfb0a049bb71d538f46fc15
Nyfaria DragonMoose <kinaria3324@gmail.com>
2023-07-16 15:18:04 -0500

    full 1.20
    Took 7 minutes

--- Forge/src/main/java/com/nyfaria/nyfsmultiloader/datagen/ModLootTableProvider.java
+++ Forge/src/main/java/com/nyfaria/nyfsmultiloader/datagen/ModLootTableProvider.java
@@ -3,8 +3,9 @@ package com.nyfaria.nyfsmultiloader.datagen;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.storage.loot.LootDataId;
+import net.minecraft.world.level.storage.loot.LootDataType;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.LootTables;
 import net.minecraft.world.level.storage.loot.ValidationContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
@@ -22,6 +23,7 @@ public class ModLootTableProvider extends LootTableProvider {
 
     @Override
     protected void validate(Map<ResourceLocation, LootTable> map, ValidationContext validationContext) {
-        map.forEach((id, table) -> LootTables.validate(validationContext, id, table));
+        map.forEach((id, table) ->
+                table.validate(validationContext.setParams(table.getParamSet()).enterElement("{" + id + "}", new LootDataId<>(LootDataType.TABLE, id))));
     }
 }
